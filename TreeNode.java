5133fc7a8d4811cd37eaae4083b7ab2d51253878
Bamzhy <bamzhy13@gmail.com>
2020-01-25 00:10:29 +0800

    today's mission

---
+++ TreeNode.java
@@ -0,0 +1,14 @@
+package com.bamzhy.My_LeetCode;
+
+
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+
+}
